cb6588230548f249c350f77e52f88e023316c3cb
Raj Kumar <33460256+Attached2Android@users.noreply.github.com>
2018-06-26 23:51:49 +0530

    Update apply.sh

--- A2A/apply.sh
+++ A2A/apply.sh
@@ -2,43 +2,43 @@
  cd ../../../..
  
  cd packages/apps/Settings
- patch -p1 -b < ../../../device/LAVA/IRISX8/patches/dev.patch
+ patch -p1 -b < ../../../device/LAVA/IRISX8/A2A/dev.patch
  git clean -f -d
  
  cd ../../../system/core
- patch -p1 < ../../device/LAVA/IRISX8/patches/core.patch
+ patch -p1 < ../../device/LAVA/IRISX8/A2A/core.patch
  
  cd ../../system*/bt
- patch -p1 < ../../device/LAVA/IRISX8/patches/bt.patch
+ patch -p1 < ../../device/LAVA/IRISX8/A2A/bt.patch
  
  cd ../net*
- patch -p1 < ../../device/LAVA/IRISX8/patches/netd2.patch
- patch -p1 < ../../device/LAVA/IRISX8/patches/netd1.patch
+ patch -p1 < ../../device/LAVA/IRISX8/A2A/netd2.patch
+ patch -p1 < ../../device/LAVA/IRISX8/A2A/netd1.patch
  
  cd ../vo*
- patch -p1 < ../../device/LAVA/IRISX8/patches/vold.patch
+ patch -p1 < ../../device/LAVA/IRISX8/A2A/vold.patch
  
  cd ../../frame*/av
- patch -p1 < ../../device/LAVA/IRISX8/patches/av.patch
+ patch -p1 < ../../device/LAVA/IRISX8/A2A/av.patch
  
  cd ../base
- patch -p1 < ../../device/LAVA/IRISX8/patches/base.patch
+ patch -p1 < ../../device/LAVA/IRISX8/A2A/base.patch
  
  cd ../rs
- patch -p1 < ../../device/LAVA/IRISX8/patches/rs.patch
+ patch -p1 < ../../device/LAVA/IRISX8/A2A/rs.patch
  
  cd ../native
- patch -p1 < ../../device/LAVA/IRISX8/patches/native.patch
+ patch -p1 < ../../device/LAVA/IRISX8/A2A/native.patch
  
  cd ../opt/te*/
- patch -p1 < ../../../device/LAVA/IRISX8/patches/telephony.patch
+ patch -p1 < ../../../device/LAVA/IRISX8/A2A/telephony.patch
  
  cd ../../../exte*/sepolicy
- patch -p1 < ../../device/LAVA/IRISX8/patches/sepolicy.patch
+ patch -p1 < ../../device/LAVA/IRISX8/A2A/sepolicy.patch
  
  cd ../wpa*
- patch -p1 < ../../device/LAVA/IRISX8/patches/wpa.patch
+ patch -p1 < ../../device/LAVA/IRISX8/A2A/wpa.patch
  
  cd ../..
  
- echo Patches Applied Successfully!
+ echo Applied Successfully!
